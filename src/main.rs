c8477fb8b74b0dd3f75d187e9632f6e1f074008e
GeniusJinHo <pjh1204c7@gmail.com>
2024-03-13 16:34:54 +0900

    updown game

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,53 @@
-static VAL_FIRST_RAND: i32 = 7;
-static mut VAL_DELTA_RAND: i32 = 17;
+use std::{io, u32};
+
+static mut VAL_DELTA_RAND: u32 = 17;
 
 fn main() {
-    for i in 0..10 {
-        out_rand(99);
-        unsafe {
-            println!("result = {0} | loop number = {1}", VAL_DELTA_RAND, i);
+    start_game();
+}
+
+fn start_game() {
+    println!("Input rand seed:");
+    let mut input_string = String::new();
+    io::stdin().read_line(&mut input_string).expect("Failed to read input");
+    let rand_seed: Result<u32, _> = input_string.trim().parse();
+
+    match rand_seed {
+        Ok(seed) => {
+            finder_rand(output_rand(99, seed));
+        }
+        Err(err) => {
+            println!("Wrong seed Retry to start");
+            start_game();
         }
     }
 }
 
-fn out_rand(max: i32) {
-    unsafe { VAL_DELTA_RAND = ((VAL_FIRST_RAND * VAL_DELTA_RAND + VAL_FIRST_RAND) + 1) % max };
+fn output_rand(max: u32, seed: u32) -> u32 {
+    unsafe { 
+        VAL_DELTA_RAND = ((seed * VAL_DELTA_RAND + seed) + 1) % max;
+        VAL_DELTA_RAND
+    }
+}
+
+fn finder_rand(secret_number: u32) {
+    loop {
+        println!("Input answer:");
+        let mut input_string = String::new();
+        io::stdin().read_line(&mut input_string).expect("Failed to read input");
+        let guess_number: u32 = match input_string.trim().parse() {
+            Ok(guess) => guess,
+            Err(err) => {
+                continue;
+            }
+        };
+        if guess_number == secret_number {
+            println!("정답입니다. 당신이 입력한 수: {} | 정답 수: {}", guess_number, secret_number);
+            break;
+        } else if guess_number > secret_number {
+            println!("정답 수는 당신이 입력한 수: {} 보다 낮습니다.", guess_number);
+        } else {
+            println!("정답 수는 당신이 입력한 수: {} 보다 높습니다.", guess_number);
+        }
+    }
 }
